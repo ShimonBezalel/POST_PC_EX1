cd40ea8b5353a26e288be9fb4ac2b4b7966180ec
shimonheimowitz <heimy4prez@gmail.com>
2019-04-06 15:39:14 +0300

    recycle view - implementation in progress

--- app/src/main/java/com/example/texting/MessageView.java
+++ app/src/main/java/com/example/texting/MessageView.java
@@ -1,4 +1,32 @@
 package com.example.texting;
 
+import java.util.ArrayList;
+
 public class MessageView {
+
+    private String msg;
+
+    public MessageView(String msg) {
+        this.msg = msg;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+
+    private static int lastContactId = 0;
+
+    public static ArrayList<MessageView> createMessageList() {
+        ArrayList<MessageView> messageList = new ArrayList<MessageView>();
+
+        return messageList;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+//        if (this == other) return true;
+        if (other == null ||this.getClass() != other.getClass()) return false;
+        return this.msg.equals(((MessageView)other).msg);
+    }
 }
