54cdbdb05854bc9a663813c8fba2b502021fbd6c
shimonheimowitz <heimy4prez@gmail.com>
2019-06-01 12:14:38 +0300

    message body update for db

--- app/src/main/java/com/example/texting/Message.java
+++ app/src/main/java/com/example/texting/Message.java
@@ -5,10 +5,13 @@ import android.os.Parcelable;
 
 import java.util.ArrayList;
 import java.util.Objects;
+import java.util.Date;
 
 import androidx.room.ColumnInfo;
 import androidx.room.Entity;
 import androidx.room.PrimaryKey;
+import androidx.room.Ignore;
+
 import java.util.UUID;
 
 
@@ -17,6 +20,21 @@ public class Message implements Parcelable{
     @PrimaryKey(autoGenerate = true)
     public int user_id;
 
+    @ColumnInfo(name = "manufacturer")
+    private String manufacturer;
+
+    @ColumnInfo(name= "model")
+    private String model;
+
+    @ColumnInfo(name= "timestamp")
+    private String timestamp;
+
+    @ColumnInfo(name= "date")
+    private String date;
+
+    @Ignore
+    Message(){}
+
     @ColumnInfo(name= "msg")
     private String msg;
 
@@ -24,6 +42,13 @@ public class Message implements Parcelable{
     private String id;
 
     private Message(Parcel in) {
+
+        this.timestamp =    in.readString();
+        this.date =    in.readString();
+
+        this.model =        in.readString();
+        this.manufacturer = in.readString();
+
         msg = in.readString();
         id = UUID.randomUUID().toString();
     }
@@ -32,6 +57,15 @@ public class Message implements Parcelable{
         this.msg = msg;
         UUID uid = UUID.randomUUID();
         this.id = uid.toString();
+
+        // timestamp + date
+        this.date = new Date().toString();
+
+        // Phone details from OS
+        this.model = android.os.Build.MODEL;
+        this.manufacturer = android.os.Build.MANUFACTURER;
+
+
     }
 
     String getMsg() {
@@ -64,7 +98,48 @@ public class Message implements Parcelable{
     @Override
     public void writeToParcel(Parcel dest, int flags) {
         dest.writeString(msg);
+
+        dest.writeString(this.date);
+        dest.writeString(this.timestamp);
+
+        dest.writeString(this.model);
+        dest.writeString(this.manufacturer);
+
+
     }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(String timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+
 
     public static final Parcelable.Creator<Message> CREATOR = new Parcelable.Creator<Message>() {
         public Message createFromParcel(Parcel in) {
